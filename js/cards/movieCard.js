8185e2660b43b18c27f6521b7fdd0d0a034e85b1
DeangeloCozier <DeangeloCozier03@gmail.com>
2026-08-15 06:58:31 -0400

    Added Overlays correctly for the list view on the movies webpage

--- js/cards/movieCard.js
+++ js/cards/movieCard.js
@@ -158,40 +158,46 @@ function createMovieListCard(movie) {
     return `
         <div class="listCard" dataID="${movie.id}">
 
-            <div class="listCardOverlay">
-                    <div class="leftIconList">
-                        <span class="mainType ${getMainClass(movie.mainType)}"> 
-                            ${movie.mainType} 
-                        </span>
-                    </div>
+            <div class="cardOverlay">
+                <div class="leftIconList">
+                    <span class="mainType ${getMainClass(movie.mainType)}"> 
+                        ${movie.mainType} 
+                    </span>
+                </div>
 
-                    <div class="rightIconList">
-                        ${
-                            movie.favorite
-                            ? `<span class="favoriteTrue">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="22" viewBox="0 0 3.97 5.82"><path d="M.26 0A.26.26 0 0 0 0 .26v5.56L1.98 4.5l1.99 1.32V.26A.26.26 0 0 0 3.7 0z" style="fill:currentColor;fill-opacity:1;stroke:none;stroke-width:.396875;stroke-linecap:round;stroke-linejoin:round;paint-order:stroke markers fill"/></svg>
+                <div class="rightIconList">
+                    ${
+                        movie.favorite
+                        ? `<span class="favoriteTrue">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="22" viewBox="0 0 3.97 5.82"><path d="M.26 0A.26.26 0 0 0 0 .26v5.56L1.98 4.5l1.99 1.32V.26A.26.26 0 0 0 3.7 0z" style="fill:currentColor;fill-opacity:1;stroke:none;stroke-width:.396875;stroke-linecap:round;stroke-linejoin:round;paint-order:stroke markers fill"/></svg>
+                        </span>`
+                        : `<span class="favoriteFalse">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="22" viewBox="0 0 3.97 5.82"><path d="M.26 0A.26.26 0 0 0 0 .26v5.56L1.98 4.5l1.99 1.32V.26A.26.26 0 0 0 3.7 0z" style="fill:currentColor;fill-opacity:1;stroke:none;stroke-width:.396875;stroke-linecap:round;stroke-linejoin:round;paint-order:stroke markers fill"/></svg>
+                        </span>`
+                    }
+
+                    ${
+                        movie.rating == null
+                        ? `<span class="rating ${getRatingClass(movie.rating)}">
+                                -.-
                             </span>`
-                            : `<span class="favoriteFalse">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="22" viewBox="0 0 3.97 5.82"><path d="M.26 0A.26.26 0 0 0 0 .26v5.56L1.98 4.5l1.99 1.32V.26A.26.26 0 0 0 3.7 0z" style="fill:currentColor;fill-opacity:1;stroke:none;stroke-width:.396875;stroke-linecap:round;stroke-linejoin:round;paint-order:stroke markers fill"/></svg>
+                        :  `<span class="rating ${getRatingClass(movie.rating)}">
+                                ${movie.rating.toFixed(1)}
                             </span>`
-                        }
+                    }
 
-                        ${
-                            movie.rating == null
-                            ? `<span class="rating ${getRatingClass(movie.rating)}">
-                                    -.-
-                                </span>`
-                            :  `<span class="rating ${getRatingClass(movie.rating)}">
-                                    ${movie.rating.toFixed(1)}
-                                </span>`
-                        }
+                </div>
 
+                <img src="${poster}" alt="${movie.title}" class="listImage">    
+
+                <div class="progress">
+                    <div
+                        class="progress-fill"
+                        style="width:${movie.progress}%">
                     </div>
-
-                    <img src="${poster}" alt="${movie.title}" class="listImage">    
-
-                    
+                </div>
             </div>
+
 
             <div class="listCardInformation">
                 <p class="smallListTitle">${movie.title}</p>
